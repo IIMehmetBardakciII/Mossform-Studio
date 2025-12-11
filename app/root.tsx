fd36c382e787c9a1cb74d17f65000987d17e9a3b
MehmetBardakci <m.bardakciw@gmail.com>
2025-12-11 07:27:27 +0300

    chore: setup base design tokens

--- app/root.tsx
+++ app/root.tsx
@@ -19,7 +19,11 @@ export const links: Route.LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href:
+      "https://fonts.googleapis.com/css2?" +
+      "family=Bricolage+Grotesque:wght@500;600;700&" +
+      "family=Montserrat:wght@500;600;700&" +
+      "display=swap",
   },
 ];
 
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,9 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+const HomePage = () => {
+  return (
+    <div className="display-xl">
+      HomePage
+    </div>
+  )
 }
 
-export default function Home() {
-  return <Welcome />;
-}
+export default HomePage
