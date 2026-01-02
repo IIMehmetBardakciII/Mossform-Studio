b65c8507b197c8bc9c5e5c7f9bc312ada04a0437
MehmetBardakci <m.bardakciw@gmail.com>
2026-01-02 12:34:54 +0300

    attemp2 netlify redirect problem

--- app/routes/works.tsx
+++ app/routes/works.tsx
@@ -1,11 +1,15 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { Draggable, ScrollTrigger } from "gsap/all";
 import { useRef, useState } from "react";
 import { Link } from "react-router";
 import Navbar from "~/components/Utilities/Navbar";
 import { projects } from "~/constant";
 import type { Route } from "../+types/root";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Draggable from "gsap/dist/Draggable";
+if (typeof window !== 'undefined') {
+  gsap.registerPlugin(ScrollTrigger, Draggable);
+}
 gsap.registerPlugin(ScrollTrigger, Draggable);
 const ImageContainer = ({ src, alt }: { src: string; alt: string }) => {
   return <img src={src} alt={alt} className="w-full object-cover h-full" />;
